c6c833813a87d4a572aa4131c6450cae707aafea
Moldo875 <moldovansilviu899@gmail.com>
2024-11-07 19:40:19 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+
+function startQuestions() {
+    document.getElementById("questions").style.display = "block";
+  }
+  
+  function answerYes() {
+    document.getElementById("questions").style.display = "none";
+      document.getElementById("que").style.display = "none";
+      document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "Ma bucur ca ai ales sa accepti?";
+    showConfetti()
+}
+  
+  function answerNo() {
+    document.getElementById("questions").style.display = "none";
+    document.getElementById("que").style.display = "none";
+    document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "Din pacate nu exista obtiunea de a refuza?";
+    showConfetti()
+
+}
+  
+// Function to show confetti
+function showConfetti() {
+    const confettiSettings = {
+      target: 'confetti-canvas',
+      respawn: true,
+      colors: ['#ff0000', '#00ff00', '#0000ff'], 
+    };
+  
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+    setTimeout(() => {
+      confetti.clear();
+    }, 5000);
+  }
+  
